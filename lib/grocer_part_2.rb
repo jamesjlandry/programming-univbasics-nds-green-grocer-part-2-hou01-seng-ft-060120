4117ea3217568bf8c0b0107e69345f59927bef9a
James Landry <james.j.landry@outlook.com>
2020-06-01 19:43:56 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,19 @@
+require "pry"
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  updated_cart = []
+  cart.each do |items|
+    if items[:item] == coupons.each do |yellow_tags|
+      [:item] && items[:count] >= yellow_tags[:num]
+      discount_amount = yellow_tags[:cost] / yellow_tags[:num]
+      binding.pry
+    end
+    end
+  end
 end
 
 def apply_clearance(cart)
